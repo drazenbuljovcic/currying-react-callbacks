a1ff850ba5303577820d0f545e6b629e35616739
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2018-10-27 10:51:44 +0200

    Updates

---
+++ precache-manifest.2ad02c83acec35a56586768d76ea1991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "393d43d6a8a42bfbe6d2",
+    "url": "/currying-react-event-callbacks/static/js/runtime~main.393d43d6.js"
+  },
+  {
+    "revision": "a92e1c913df23d336f10",
+    "url": "/currying-react-event-callbacks/static/js/main.a92e1c91.chunk.js"
+  },
+  {
+    "revision": "79777bf659be2ab68bda",
+    "url": "/currying-react-event-callbacks/static/js/1.79777bf6.chunk.js"
+  },
+  {
+    "revision": "a92e1c913df23d336f10",
+    "url": "/currying-react-event-callbacks/static/css/main.4c4f9bc1.chunk.css"
+  },
+  {
+    "revision": "a8adebcb67a59d6efa3ba495205531f5",
+    "url": "/currying-react-event-callbacks/index.html"
+  }
+];
